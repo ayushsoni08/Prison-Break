7765c7e75b441ad968edaff72dde810d3aad2d94
Ayush Soni <ayushsoni0718@gmail.com>
2026-07-29 20:27:51 +0530

    02

---
+++ mission-python/strings.py
@@ -0,0 +1,11 @@
+#Ask user for their name
+#input method is used to take user-input
+name = input('What is your name? ')
+
+#string methods
+# 1. Strip: Remove whitespaces from string
+# name = name.strip();
+
+#ways to display
+# print("Hello", name)
+print(f"Hello, {name}")
